16dff5dcf7614321fb98ef403c364209597e09a6
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-07-27 19:05:04 +0300

    more fixes

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -10,12 +10,12 @@ const Discover = () => {
   const { topic } = router.query;
 
   const activeTopicStyle =
-    "xl:border-b-2 hover:bg-primary xl:border-gray-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]";
+    "xl:border-2 hover:bg-primary xl:border-gray-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]";
   const topicStyle =
-    "xl:border-b-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+    "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
 
   return (
-    <div className="xl:border-2 xl:border-gray-200 pb-6">
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
       <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
         Populat Topics
       </p>
